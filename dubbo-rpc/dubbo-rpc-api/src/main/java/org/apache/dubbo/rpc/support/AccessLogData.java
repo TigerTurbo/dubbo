b1fd2d17de00204c2128674c8b5ba014e67038d0
keshi.ys <keshi.ys@alibaba-inc.com>
2022-04-22 17:50:54 +0800

    fix #9953

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/support/AccessLogData.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/support/AccessLogData.java
@@ -240,7 +240,7 @@ public final class AccessLogData {
 
         Object[] args = get(ARGUMENTS) != null ? (Object[]) get(ARGUMENTS) : null;
         if (args != null && args.length > 0) {
-            sn.append(JSON.toJSONString(args, SerializerFeature.IgnoreNonFieldGetter));
+            sn.append(JSON.toJSONString(args, SerializerFeature.IgnoreErrorGetter));
         }
 
         return sn.toString();
